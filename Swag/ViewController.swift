a348dcfa2880800a6d8ed798594789c4018fc714
Madian <Madian@khaleds-mac.local>
2019-02-13 15:48:39 +0200

    Initial Commit

---
+++ Swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swag
+//
+//  Created by Madian on 2/13/19.
+//  Copyright © 2019 Madian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
